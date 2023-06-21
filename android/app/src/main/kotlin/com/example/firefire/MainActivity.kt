a78f9cf2eac3d5515ebb96f3cab68b84f7ef5e55
leonardo.bocon <leonardo.bocon@manticore-labs.com>
2023-06-20 21:46:49 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/firefire/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firefire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firefire/pages/add_name_page.dart';
+import 'package:firefire/pages/edit_name_page.dart';
+import 'package:firefire/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/add': (context) => const AddNamePage(),
+        '/edit': (context) => const EditNamePage(),
+      },
+    );
+  }
+}
